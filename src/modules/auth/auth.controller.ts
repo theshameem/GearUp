d5d5ebce385e8cef872b1ae1ea372dd33b0440f8
theshameem <theshameem.alam@gmail.com>
2026-07-14 16:58:19 +0600

    feat: user login api implementation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -18,7 +18,32 @@ const registerUser = catchAsync(async (req: Request, res: Response) => {
 });
 
 const loginUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "User logged in successfully",
+      data: { accessToken, refreshToken },
+    });
+  },
 );
 
 const myDetails = catchAsync(
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
+import { SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { jwtUtils } from "../../utils/jwt";
 import { ILoginUser, RegisterUserPayload } from "./auth.interface";
 
 const registerUserIntoDB = async (payload: RegisterUserPayload) => {
@@ -37,7 +39,46 @@ const registerUserIntoDB = async (payload: RegisterUserPayload) => {
   return createdUser;
 };
 
-const loginUser = async (payload: ILoginUser) => {};
+const loginUser = async (payload: ILoginUser) => {
+  const { email, password } = payload;
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      email,
+    },
+  });
+
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched) {
+    throw new Error("Password is incorrect");
+  }
+
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    role: user.role,
+    email: user.email,
+    status: user.status,
+    profileImage: user.profileImage,
+    address: user.address,
+    phone: user.phone,
+  };
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
+  return { accessToken, refreshToken };
+};
 
 const loggedInUserDetails = async (userId: string) => {};
 
