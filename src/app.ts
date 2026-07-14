d63a823cc7e7f7dd1feb20627201fbfcd52e6aac
theshameem <theshameem.alam@gmail.com>
2026-07-14 13:08:26 +0600

    feat: gearup project initial setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+
+// app.use(
+//   cors({
+//     origin: config.app_url,
+//     credentials: true,
+//   }),
+// );
+
+export default app;
