509d842636aaf848898cc57a437a10601120779c
theshameem <theshameem.alam@gmail.com>
2026-07-14 16:31:16 +0600

    feat: global error handler implementation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application } from "express";
 import config from "./config";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { authRoutes } from "./modules/auth/auth.route";
 
 const app: Application = express();
@@ -18,5 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
+app.use(globalErrorHandler);
 
 export default app;
